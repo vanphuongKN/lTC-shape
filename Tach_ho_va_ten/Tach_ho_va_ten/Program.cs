c4d4ac9e3effc580fadfcb7ef0f425a608319922
Van Phuong <email_cua_ban@gmail.com>
2026-01-29 20:05:38 +0700

    upload bài 7

---
+++ Tach_ho_va_ten/Tach_ho_va_ten/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Tach_ho_va_ten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vo Van Phuong 2415053122230");
+            Console.WriteLine("Nhap ho ten");
+            string fullName = Console.ReadLine();
+            if (string.IsNullOrEmpty(fullName))
+            {
+                Console.WriteLine(("khong tach"));
+            }
+            string[] words = fullName.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+            if (words == null || words.Length == 0)
+            {
+                Console.WriteLine("mang kh duoc null");
+            }
+            else
+            {
+                foreach (string word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
