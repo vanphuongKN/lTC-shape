983f24c375280ac42edc73f4d7e7c622a6cddbc5
Van Phuong <email_cua_ban@gmail.com>
2026-01-29 20:07:58 +0700

    upload bài 9

---
+++ Dem_so_chuoi_trong_mang/Dem_so_chuoi_trong_mang/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Dem_so_chuoi_trong_mang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vo Van Phuong 2415053122230");
+            string[] danhSach = { "vovanphuong", null, "", "Lap trinh c#", null, "", "sagfdchhtdr" };
+            int count = 0;
+            foreach (string s in danhSach)
+            {
+                if (s != null && s != "")
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"so phan tu khac null và khac rong: {count}");
+
+        }
+    }
+}
