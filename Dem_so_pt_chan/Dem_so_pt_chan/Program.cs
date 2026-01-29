b3e0dcce3d2a1411c366eabc73bcb2b8958c1f6a
Van Phuong <email_cua_ban@gmail.com>
2026-01-29 20:03:39 +0700

    upload bài 6

---
+++ Dem_so_pt_chan/Dem_so_pt_chan/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Dem_so_pt_chan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vo Van Phuong 2415053122230");
+            Console.WriteLine("nhap so phan tu");
+            int n;
+            if(!int.TryParse(Console.ReadLine(), out n) || n < 0)
+            {
+                Console.WriteLine("hay nhap lai so phan tu");
+
+            }
+            int[] numbers = new int[n];
+            if (numbers == null) {
+                Console.WriteLine("so phan tu chan là : 0");
+
+            }
+                
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"nhap phan tu {i + 1}:");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($" So phan tu chan la : {count}");
+        }
+    }
+}
