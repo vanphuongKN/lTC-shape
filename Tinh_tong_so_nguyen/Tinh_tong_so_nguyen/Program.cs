579e0590f2e9e6c695a73b03c3847a94231dc84b
Van Phuong <email_cua_ban@gmail.com>
2026-01-29 18:23:07 +0700

    upload bài 4

---
+++ Tinh_tong_so_nguyen/Tinh_tong_so_nguyen/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Tinh_tong_so_nguyen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vo Van Phuong 2415053122230");
+            Console.WriteLine("Nhap so phan tu n:");
+            int n;
+            if (!int.TryParse(Console.ReadLine(), out n) || n < 0)
+            {
+                Console.WriteLine("hay nhap so phan tu");
+            }
+            int[] numbers = new int[n];
+            if (numbers == null)
+            {
+                Console.WriteLine("Mang chua khoi tao");
+                return;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"nhap pt thu {i + 1}:");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            for (int i = 0;i < numbers.Length; i++) {
+                sum += numbers[i];
+                }
+            Console.WriteLine($"Tpng la: {sum}");
+        }
+    }
+}
